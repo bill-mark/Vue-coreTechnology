a4369698b0300776d1c769d40725235ddf5a8971
bill.mark <weiguicheng@foxmail.com>
2018-06-15 17:28:52 +0800

    up

--- practice/instance/index.js
+++ practice/instance/index.js
@@ -1,12 +1,6 @@
 import Vue from 'vue'
 
-// new Vue({
-// 	el:'#root',
-// 	template:'<div>this is content</div>'
-// })
-
 const app = new Vue({
-	//el:'#root',
 	template:'<div>{{text}}</div>',
 	data:{
 		text:0
@@ -17,8 +11,4 @@ app.$mount('#root')
 
 console.log(app.$data)
 console.log(app.$pros)
-console.log(app.$el)
-
-// setInterval( ()=>{
-// 	app.text += 1
-// },1000 )
+console.log(app.$root)
